34e5e00a2dd52565c114e1433069b206724d9673
Abraham Kuri <kurenn@icalialabs.com>
2016-07-25 18:13:17 -0500

    Finishes landing page

--- vendor/assets/javascripts/custom.js
+++ vendor/assets/javascripts/custom.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
  =============================================== */
 
 $(function() {
-  $('.scrollto a[href*=#]:not([href=#])').click(function() {
+  $('.scrollto a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
